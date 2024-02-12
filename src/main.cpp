7081574fedfb3d4c1e5a19b452bea6595ac822cc
Ethancao123 <ethan.cao123456@gmail.com>
2024-02-11 21:00:15 -0600

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #include <Arduino.h>
 #include <SimpleFOC.h>
 
+//#define DEBUG
 #define MAX_RPS 300
 #define TIMEOUT_MILLIS 50
 #define DEADZONE 3
-// #define DEBUG
 #define REVERSED 1 //Used in macros 1 for conventional, -1 for reverse
-
-#define TORQUE_RANGE 12//from negative to positive
-#define ANGLE_RANGE 10//from negative to positive but centered around 2
-
+#define TORQUE_RANGE 12 //from negative to positive
+#define ANGLE_RANGE 10 //from negative to positive but centered around 2
 
 // BLDC motor instance BLDCMotor(polepairs, (R), (KV 1100))
 BLDCMotor motor = BLDCMotor(7, 0.1, 1750, 0.01/1000);
-
 BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
-
-// position / angle sensor instance
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5048_I2C);
-
-// inline current sense instance InlineCurrentSense(R, gain, phA, phB, phC)
 LowsideCurrentSense currentsense = LowsideCurrentSense(0.003, -64.0/7.0, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
 
 // commander instance
@@ -28,13 +21,11 @@ Commander command = Commander(Serial);
 void doTarget(char* cmd){command.motor(&motor, cmd);}
 #endif
 
+int maxPowerMillis;
+float hammerTorque;
 volatile uint16_t servoPulse = 1500;
 volatile uint32_t last_isr = 0;
 bool motoren = false;
-
-int maxPowerMillis;
-float hammerTorque;
-
 int Hcommand = 0;
 
 #ifndef DEBUG
@@ -50,18 +41,6 @@ void ServoPulseUpdate() {
         last_isr = millis();
     }
 }
-
-void TriggerPulseUpdate() {
-    static uint32_t startTime = 0;
-    uint32_t curTime = micros();
-    if (digitalRead(A_PWM)) // Pin transitioned from low to high
-        startTime = curTime; // Start counting pulse time
-    else // Pin transitioned from high to low
-        servoPulse = (uint16_t)(curTime - startTime);
-    if(servoPulse <= 2100 && servoPulse >= 900) {
-        last_isr = millis();
-    }
-}
 #endif
 
 void initArm(){
@@ -70,9 +49,9 @@ void initArm(){
     motor.current_limit = 8;
     MotionControlType previous = motor.controller;
     motor.controller = MotionControlType::velocity;
-    while(millis() - startMillis < 1000){
+    while(millis() - startMillis < 1500){
         motor.loopFOC();
-        motor.move(-8 * REVERSED);
+        motor.move(-10 * REVERSED);
     }
     motor.sensor_offset = motor.shaft_angle;
     motor.current_limit = prevLimit;
@@ -82,13 +61,9 @@ void initArm(){
     motor.controller = previous;
 }
 
-
-
 void doHammer(char *a) {
     Hcommand = atoi(a);
 }
-
-
 void setup() {
     #ifdef DEBUG
     Serial.begin(115200);
@@ -117,16 +92,10 @@ void setup() {
     motor.voltage_sensor_align = 1.25;
     motor.velocity_index_search = 6;
 
-
-
-    // set motion control type to torque (default)
     motor.controller = MotionControlType::angle;
-
-    // set torque control type to voltage (default)
     motor.torque_controller = TorqueControlType::voltage;
-
-    // set FOC modulation type to sinusoidal
     motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+
 
     // velocity PID controller
     motor.PID_velocity.P = 3;
@@ -136,28 +105,20 @@ void setup() {
     motor.PID_velocity.limit = 20;
     motor.LPF_velocity.Tf = 0.07;
 
-
     motor.P_angle.P = 6;
     motor.P_angle.I = 0.2;
     motor.P_angle.D = 0.05;
     motor.P_angle.output_ramp = 500;
 
     motor.LPF_angle = 0.1;
-
-    // set motor voltage limit, this limits Vq
     motor.voltage_limit = 18;
-    // set motor velocity limit
     motor.velocity_limit = 70000;
-    // set motor current limit, this limits Iq
     motor.current_limit = 8;
 
     maxPowerMillis = 250;
     hammerTorque = 50;
-    
-    // initialize motor
-    motor.init();
 
-    // align sensor and start FOC
+    motor.init();
     motor.initFOC();
 
     pinMode(A_PWM, INPUT_PULLDOWN);
@@ -201,10 +162,8 @@ void loop() {
         motor.enable();
         motoren = true;
     } 
-
     if(pulse < 1343 && pulse > 1085) { //lower range
         motor.controller = MotionControlType::torque;
-        int t = TORQUE_RANGE;
         target = map(pulse,1086,1342,-1*TORQUE_RANGE, TORQUE_RANGE);
     }
     else if(pulse < 1916 && pulse > 1658) { //higher range
